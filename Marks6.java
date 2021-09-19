7953e806ff3e4e93f839ed09c5886e98d899dba4
kapilmukhi123 <kapilmukhi37@gmail.com>
2021-09-19 00:40:40 -0700

    Project by you & me

---
+++ Marks6.java
@@ -0,0 +1,35 @@
+public class Marks6{
+
+    public static void main(String[] args) {
+
+    int math=80;
+	int english=50;
+	int java=80;
+	int be=68;
+	int chemistry=77;
+	float myresult= math+english+java+be+chemistry;
+	float percentage= (myresult/500)*100;
+	
+    if (percentage>85){
+		
+	System.out.println("your result is A plus:"+percentage);
+	}
+	
+if(percentage<=75){
+	System.out.println("you got A:"+percentage);
+}
+
+if(percentage<=60){
+	
+System.out.println("you got  b grade:"+percentage);
+}
+
+if(percentage<=50){
+	System.out.println("Fail:"+percentage);
+}
+
+
+	}
+}
+
+
